44ad9303b46bf60753e8534cdbd92765342d9b77
codac-proc <codac.proc@gmail.com>
2025-01-25 22:07:09 +0900

    updated to squeeze count

--- app/src/main/java/jp/math/anti/lemonade/MainActivity.kt
+++ app/src/main/java/jp/math/anti/lemonade/MainActivity.kt
@@ -8,16 +8,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
